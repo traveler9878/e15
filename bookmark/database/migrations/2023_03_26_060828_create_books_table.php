be1f8fa57fbe67847eb14230ce707f97ac66c784
David Curtis <traveler9878@gmail.com>
2023-03-28 12:13:56 +0000

    Database week

---
+++ bookmark/database/migrations/2023_03_26_060828_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('slug');
+            $table->string('title');
+            $table->smallInteger('published_year');
+            $table->string('cover_url');
+            $table->string('info_url');
+            $table->string('purchase_url');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
--- bookmark/routes/web.php
+++ bookmark/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PracticeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::any('/practice/{n?}', [PracticeController::class, 'index']);
 Route::get('/', function () {
     view('welcome');
 });
